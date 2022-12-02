067605b35812d2cf337d839c486b788f74b0fe8a
Loloxon <nikodemko@interia.pl>
2022-12-02 01:22:59 +0100

    done

--- lab_5_zad/main.py
+++ lab_5_zad/main.py
@@ -1,14 +1,16 @@
 from graphviz import Digraph
-import re
+from graphviz import Graph
+import os
+os.environ["PATH"] += os.pathsep + r"C:\Users\Loloxon\AppData\Local\Programs\Python\Python310\Graphviz\bin"
 
 eq = []
 A = ""
 w = ""
 
 
-def loader(input):
+def loader(idx):
     global A, w
-    with open(input) as f:
+    with open("input" + idx + ".txt", encoding="utf8") as f:
         lines = f.readlines()
     for l in lines:
         if l[0] == "(":
@@ -17,8 +19,12 @@ def loader(input):
             A = l[5:-2]
         elif l[0] == 'w':
             w = l[4:]
-    A = re.sub('[, ]', '', A)
-    w = re.sub('[, ]', '', w)
+    for c in A:
+        if c in '[, ]':
+            c = ""
+    for c in w:
+        if c in '[, ]':
+            c = ""
 
 
 D = []
@@ -39,8 +45,11 @@ def dependence():
                 I.append((e1[1], e2[1]))
 
 
-def drawGraph(word, alphabet, D):
+def drawGraph(word, alphabet, D, idx):
+    global output
+
     dot = Digraph()
+    # dot.format(format='png')
     dict = {}
     new_word = []
     for a in alphabet:
@@ -49,63 +58,99 @@ def drawGraph(word, alphabet, D):
         dot.node(w + str(dict[w]), w + str(dict[w]))
         new_word.append(w + str(dict[w]))
         dict[w] = dict[w] + 1
-    # dot.edges(['AB', 'AB', 'AB', 'BC', 'BA', 'CB'])
     edges = []
 
     for w1 in range(len(word)):
         for w2 in range(w1 + 1, len(word)):
             for d in D:
                 if (word[w1], word[w2]) == d:
-                    # dot.edge(str(new_word[w1]), str(new_word[w2]))
                     edges.append((str(new_word[w1]), str(new_word[w2])))
-    print(edges)
     for e1 in edges:
         s = e1[0]
         e = e1[1]
         edges_excluding = [ed for ed in edges if ed != e1]
-        print("     for", s, e)
         if can_simplify(s, e, edges_excluding):
             edges = edges_excluding
 
     for e in edges:
         dot.edge(e[0], e[1])
 
-    # dot.edges(edges)
-    # dot.edges(dot.edges()[:-1])
+    printFNF(edges)
 
-    print(dot.source)
-    dot.render(view=True)
+    this_print = dot.source
+    output += this_print + "\n"
 
+    dot.render(format="png", filename="graph" + idx, view=True)
 
 def can_simplify(start, end, edges):
-    print(start, end, len(edges), edges)
     if start == end:
         return True
     for e in edges:
-        print(e, len(edges))
         if e[0] == start:
             next_hop = e[1]
-            if next_hop==end or can_simplify(next_hop, end, [ed for ed in edges if ed != e]):
+            if next_hop == end or can_simplify(next_hop, end, [ed for ed in edges if ed != e]):
                 return True
     return False
 
 
-def printT(D):
-    print("D = {", end="")
+def printT(D, name):
+    global output
+
+    this_print = name + " = {"
     for d in range(len(D) - 1):
-        print("(", D[d][0], ",", D[d][1], ")", sep="", end=",")
-    print("(", D[len(D) - 1][0], ",", D[len(D) - 1][1], ")", sep="", end="}\n")
+        this_print += "(" + D[d][0] + "," + D[d][1] + "),"
+    this_print += "(" + D[len(D) - 1][0] + "," + D[len(D) - 1][1] + ")}"
+    output += this_print + "\n\n"
 
+def printFNF(edges):
+    global output
 
-# input = 'input1.txt'
-input = 'input2.txt'
+    this_print = "FNF([w]) = "
 
-loader(input)
+    dist = {}
+    for e in edges:
+        dist[e[0]] = 0
+        dist[e[1]] = 0
+
+    Q = []
+    for e in edges:
+        Q.append(e[0])
+    while len(Q)>0:
+        v = Q[0]
+        Q.pop(0)
+        for e in edges:
+            if e[0] == v:
+                dist[e[1]] = max(dist[e[0]]+1, dist[e[1]])
+                Q.append(e[1])
+
+    for idx in range(len(edges)):
+        this_print += "["
+        for d in dist:
+            if dist[d] == idx:
+                this_print += d + ", "
+        if this_print[-1] == "[":
+            this_print = this_print[:-1]
+        else:
+            this_print = this_print[:-2]
+            this_print += "]"
+    output += this_print + "\n\n"
+
+def saver(idx, output):
+    f = open("output" + idx + ".txt", "w")
+    f.write(output)
+    f.close()
+
+# idx = '1'
+idx = '2'
+
+loader(idx)
 dependence()
-for e in eq:
-    print(e)
-print(A)
-print(w)
-printT(D)
-printT(I)
-drawGraph(w, A, D)
+
+output = ""
+printT(D, "D")
+printT(I, "I")
+drawGraph(w, A, D, idx)
+
+saver(idx, output)
+
+print(output)
